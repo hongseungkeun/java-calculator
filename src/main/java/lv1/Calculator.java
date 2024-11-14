2090fa53edc47f79d6f92f16b199b92f1a71a50b
SeuneKeun <eiziong1@gmail.com>
2024-11-14 16:57:13 +0900

    feat: 반복 종료 구현
    반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 코드 수정

--- src/main/java/lv1/Calculator.java
+++ src/main/java/lv1/Calculator.java
@@ -6,48 +6,58 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
-        int firstNum = 0;
-        int secondNum = 0;
+        final String EXIT = "exit";
+        String exitCheck = "";
+        int result;
+        int firstNum;
+        int secondNum;
 
-        try {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            secondNum = sc.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("양의 정수만 입력해주세요");
+        while (!exitCheck.equals(EXIT)) {
+            result = 0;
+            firstNum = 0;
+            secondNum = 0;
+
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                firstNum = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                secondNum = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("양의 정수만 입력해주세요");
+            }
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+
+                case '/':
+                    try {
+                        result = firstNum / secondNum;
+                    } catch (ArithmeticException e) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    }
+                    break;
+
+                default:
+                    System.out.println("+,-,*,/ 연산자만 가능합니다.");
+            }
+
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            exitCheck = sc.next();
         }
-
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
-
-        switch (operator) {
-            case '+':
-                result = firstNum + secondNum;
-                break;
-
-            case '-':
-                result = firstNum - secondNum;
-                break;
-
-            case '*':
-                result = firstNum * secondNum;
-                break;
-
-            case '/':
-                try {
-                    result = firstNum / secondNum;
-                } catch (ArithmeticException e) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
-                break;
-
-            default:
-                System.out.println("+,-,*,/ 연산자만 가능합니다.");
-        }
-
-        System.out.println("결과: " + result);
     }
 }
 
