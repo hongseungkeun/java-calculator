a1dffc5c618fe8fde21f31834abf92236a5982a0
SeuneKeun <eiziong1@gmail.com>
2024-11-14 16:45:20 +0900

    fix: 양의 정수만 받도록 기능 수정

--- src/main/java/lv1/Calculator.java
+++ src/main/java/lv1/Calculator.java
@@ -1,18 +1,24 @@
 package lv1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
-        int firstNum;
-        int secondNum;
+        int firstNum = 0;
+        int secondNum = 0;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        firstNum = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        secondNum = sc.nextInt();
+        try {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            firstNum = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            secondNum = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("양의 정수만 입력해주세요");
+        }
+
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
