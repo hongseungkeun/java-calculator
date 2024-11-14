de3d11e7c601e5baea483cd6fefcb6b122c13c1a
SeuneKeun <eiziong1@gmail.com>
2024-11-14 17:14:27 +0900

    refactor: 예외 발생 시 더이상 진행하지 않도록 코드 수정

--- src/main/java/lv1/Calculator.java
+++ src/main/java/lv1/Calculator.java
@@ -14,44 +14,44 @@ public class Calculator {
 
         while (!exitCheck.equals(EXIT)) {
             result = 0;
-            firstNum = 0;
-            secondNum = 0;
 
             try {
                 System.out.print("첫 번째 숫자를 입력하세요: ");
                 firstNum = sc.nextInt();
                 System.out.print("두 번째 숫자를 입력하세요: ");
                 secondNum = sc.nextInt();
+
+                if (firstNum < 0 || secondNum < 0) {
+                    throw new InputMismatchException();
+                }
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+
+                switch (operator) {
+                    case '+':
+                        result = firstNum + secondNum;
+                        break;
+
+                    case '-':
+                        result = firstNum - secondNum;
+                        break;
+
+                    case '*':
+                        result = firstNum * secondNum;
+                        break;
+
+                    case '/':
+                        result = firstNum / secondNum;
+                        break;
+
+                    default:
+                        System.out.println("+,-,*,/ 연산자만 가능합니다.");
+                }
             } catch (InputMismatchException e) {
                 System.out.println("양의 정수만 입력해주세요");
-            }
-
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-
-            switch (operator) {
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-
-                case '*':
-                    result = firstNum * secondNum;
-                    break;
-
-                case '/':
-                    try {
-                        result = firstNum / secondNum;
-                    } catch (ArithmeticException e) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    }
-                    break;
-
-                default:
-                    System.out.println("+,-,*,/ 연산자만 가능합니다.");
+            } catch (ArithmeticException e) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
             }
 
             System.out.println("결과: " + result);
