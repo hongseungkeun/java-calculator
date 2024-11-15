5c6a313777b61ebb8e4540d40f8c82691a2849a0
SeuneKeun <eiziong1@gmail.com>
2024-11-15 11:18:30 +0900

    refactor: Calculator 클래스가 연산 수행을 하도록 메소드 수정

--- src/main/java/com/sparta/calculator/Application.java
+++ src/main/java/com/sparta/calculator/Application.java
@@ -1,20 +1,19 @@
-package lv1;
+package com.sparta.calculator;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Calculator {
+public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
+
         final String EXIT = "exit";
         String exitCheck = "";
-        int result;
         int firstNum;
         int secondNum;
 
         while (!exitCheck.equals(EXIT)) {
-            result = 0;
-
             try {
                 System.out.print("첫 번째 숫자를 입력하세요: ");
                 firstNum = sc.nextInt();
@@ -28,33 +27,13 @@ public class Calculator {
                 System.out.print("사칙연산 기호를 입력하세요: ");
                 char operator = sc.next().charAt(0);
 
-                switch (operator) {
-                    case '+':
-                        result = firstNum + secondNum;
-                        break;
-
-                    case '-':
-                        result = firstNum - secondNum;
-                        break;
-
-                    case '*':
-                        result = firstNum * secondNum;
-                        break;
-
-                    case '/':
-                        result = firstNum / secondNum;
-                        break;
-
-                    default:
-                        System.out.println("+,-,*,/ 연산자만 가능합니다.");
-                }
+                System.out.println("결과: " + cal.calculate(operator, firstNum, secondNum));
             } catch (InputMismatchException e) {
                 System.out.println("양의 정수만 입력해주세요");
             } catch (ArithmeticException e) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
             }
 
-            System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             exitCheck = sc.next();
         }
