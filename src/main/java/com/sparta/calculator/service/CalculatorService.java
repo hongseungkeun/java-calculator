ff2f16fa0a35fd6e9f3943f16b1e4535dcd42a33
SeuneKeun <eiziong1@gmail.com>
2024-11-15 21:32:48 +0900

    feat: 입출력을 담당하는 클래스 추가
    - 사용자에게 입력을 받는 InputView 클래스
    - 메세지를 출력하는 OutputView 클래스

--- src/main/java/com/sparta/calculator/service/CalculatorService.java
+++ src/main/java/com/sparta/calculator/service/CalculatorService.java
@@ -1,60 +1,38 @@
 package com.sparta.calculator.service;
 
-import com.sparta.calculator.exception.BadInputException;
 import com.sparta.calculator.domain.Calculator;
+import com.sparta.calculator.exception.BadInputException;
+import com.sparta.calculator.view.InputView;
+import com.sparta.calculator.view.OutputView;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class CalculatorService {
-    private final Calculator cal;
-
-    public CalculatorService() {
-        this.cal = new Calculator();
-    }
+    private final Calculator calculator = new Calculator();
 
     public void run() {
-        Scanner sc = new Scanner(System.in);
-
-        final String EXIT = "exit";
-        final String CONFIRM_DELETE = "yes";
-        String exitCheck = "";
-
-        while (!exitCheck.equals(EXIT)) {
+        do {
             try {
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                int firstNum = sc.nextInt();
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                int secondNum = sc.nextInt();
+                int firstNum = InputView.getPositiveNum("첫 번째 숫자를 입력하세요: ");
+                int secondNum = InputView.getPositiveNum("두 번째 숫자를 입력하세요: ");
+                char operator = InputView.getOperator();
 
-                if (firstNum < 0 || secondNum < 0) {
-                    throw new InputMismatchException();
-                }
+                int result = calculator.calculate(operator, firstNum, secondNum);
 
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                char operator = sc.next().charAt(0);
+                OutputView.printResult(result);
+                OutputView.printStoredResults(calculator.getResultStorage());
 
-                int result = cal.calculate(operator, firstNum, secondNum);
-                System.out.println("결과: " + result);
-
-                System.out.println("저장된 결과들: " + cal.getResultStorage());
-
-                System.out.print("가장 오래된 결과를 삭제하시겠습니까? (yes 입력 시 삭제): ");
-                String deleteCheck = sc.next();
-
-                if (deleteCheck.equals(CONFIRM_DELETE)) {
-                    cal.removeResult();
+                if (InputView.confirmDelete()) {
+                    calculator.removeResult();
                 }
             } catch (InputMismatchException e) {
-                System.out.println("양의 정수만 입력해주세요");
+                OutputView.printlnMessage("양의 정수만 입력해주세요");
+                InputView.sc.nextLine();
             } catch (ArithmeticException e) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                OutputView.printlnMessage("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
             } catch (BadInputException e) {
-                System.out.println(e.getMessage());
+                OutputView.printlnMessage(e.getMessage());
             }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            exitCheck = sc.next();
-        }
+        } while (!InputView.confirmExit());
     }
 }
